7a96db430863dd8cc2387ed1ab1b517e0eb438d9
ad-rinaline2 <arindol0921@gmail.com>
2021-10-08 09:09:55 +0530

    1st Update

--- src/metadata.js
+++ src/metadata.js
@@ -2,52 +2,52 @@ export const Users = [
   {
     id:1,
     profilePicture: "assets/person/1.jpeg",
-    username: "Safak Kocaoglu",
+    username: "Elon Musk",
   },
   {
     id:2,
     profilePicture: "assets/person/2.jpeg",
-    username: "Janell Shrum",
+    username: "Satoshi Nakamoto",
   },
   {
     id:3,
     profilePicture: "assets/person/3.jpeg",
-    username: "Alex Durden",
+    username: "Mr. Dry_Runner",
   },
   {
     id:4,
     profilePicture: "assets/person/4.jpeg",
-    username: "Dora Hawks",
+    username: "Mr. Dry_Runner",
   },
   {
     id:5,
     profilePicture: "assets/person/5.jpeg",
-    username: "Thomas Holden",
+    username: "Mr. Dry_Runner",
   },
   {
     id:6,
     profilePicture: "assets/person/6.jpeg",
-    username: "Shirley Beauchamp",
+    username: "Mr. Dry_Runner",
   },
   {
     id:7,
     profilePicture: "assets/person/7.jpeg",
-    username: "Travis Bennett",
+    username: "Mr. Dry_Runner",
   },
   {
     id:8,
     profilePicture: "assets/person/8.jpeg",
-    username: "Kristen Thomas",
+    username: "Mr. Dry_Runner",
   },
   {
     id:9,
     profilePicture: "assets/person/9.jpeg",
-    username: "Gary Duty",
+    username: "Mr. Dry_Runner",
   },
   {
     id:10,
     profilePicture: "assets/person/10.jpeg",
-    username: "Safak Kocaoglu",
+    username: "Mr. Dry_Runner",
   },
 ];
 
